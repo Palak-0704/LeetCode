1fb8e8fd3188c1394ab3a2f3ad0d96532e556207
Palak <161704641+Palak-0704@users.noreply.github.com>
2026-02-15 02:11:38 +0530

    Time: 0 ms (100%), Space: 14.5 MB (9.5%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.cpp
+++ 1089-duplicate-zeros/1089-duplicate-zeros.cpp
@@ -1,12 +1,18 @@
 class Solution {
 public:
     void duplicateZeros(vector<int>& arr) {
+        vector<int> temp;
         for(int i=0;i<arr.size();i++){
             if(arr[i]==0){
-                arr.insert(arr.begin()+i+1,0);
-                i++;
-                arr.pop_back();
+                temp.push_back(0);
+                temp.push_back(0);
             }
+            else{
+                temp.push_back(arr[i]);
+            }
+        } 
+        for(int i=0;i<arr.size();i++){
+            arr[i]= temp[i];
         }   
     }
 };

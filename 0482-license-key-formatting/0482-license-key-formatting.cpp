df7b67ece784911389a5a2859df01fd026bef29f
Palak <161704641+Palak-0704@users.noreply.github.com>
2026-02-21 02:02:22 +0530

    Time: 4 ms (30.81%), Space: 10.8 MB (48.21%) - LeetHub

--- 0482-license-key-formatting/0482-license-key-formatting.cpp
+++ 0482-license-key-formatting/0482-license-key-formatting.cpp
@@ -5,15 +5,27 @@ public:
         for(int i=0;i<s.length();i++){
             s[i]=toupper(s[i]);
         }
-
-        int j=s.length();
-        while(j>0){
+        string res="";
+        int count=0;
+        for(int i=s.length()-1;i>=0;i--){
+            if(count==k){
+                res.push_back('-');
+                count=0;
+            }
+            res+=s[i];
+            count++;
+        }
+        reverse(res.begin(),res.end());
+/*         int j=s.length();
+        while(j-k>0){
             if(j-k>0){
                 s.insert(j-k,"-");
             }
             j=j-k;
-        }
+        } 
         return s;
+        */
+        return res;
 
     }
 };

5f0165ac1a7d6eea06a1c4515b9e2aa726eae7ac
Palak <161704641+Palak-0704@users.noreply.github.com>
2026-02-27 01:58:17 +0530

    Time: 0 ms (100%), Space: 21.2 MB (27.95%) - LeetHub

---
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isToeplitzMatrix(vector<vector<int>>& matrix) {
+        bool res= true;
+        for(int i=0;i<matrix.size()-1;i++){
+            for(int j=0;j<matrix[0].size()-1;j++){
+                if(matrix[i][j] !=matrix[i+1][j+1]){
+                    res=false;
+                }
+            }
+        }
+        return res;
+    }
+};

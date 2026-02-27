9670d8396574c104caf6924015c8e3f7f1aff093
Palak <161704641+Palak-0704@users.noreply.github.com>
2026-02-28 02:39:34 +0530

    Time: 28 ms (5.73%), Space: 31.9 MB (9.75%) - LeetHub

--- 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.cpp
+++ 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.cpp
@@ -2,22 +2,21 @@ class Solution {
 public:
     vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
         unordered_map<int,int>mp;
-        vector<int>res;
         for(int i=0;i<grid.size();i++){
             for(int j=0;j<grid[0].size();j++){
                 mp[grid[i][j]]++;
             }
         }
-        for(auto it :mp){
-            if(it.second==2){
-                res.push_back(it.first);
-            }
-        }
+        int rep;
+        int missing;
         for(int i=1;i<=grid.size()*grid.size();i++){
-            if(!mp.count(i)){
-                res.push_back(i);
+            if(mp[i]==2){
+                rep=i;
+            }
+            if(mp[i]==0){
+                missing=i;
             }
         }
-        return res;
+        return {rep,missing};
     }
 };

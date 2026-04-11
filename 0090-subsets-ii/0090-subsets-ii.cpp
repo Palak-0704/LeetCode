5d4b02e40fbd0a1da9900ba138e3a2215dc93877
Palak <161704641+Palak-0704@users.noreply.github.com>
2026-04-12 04:05:14 +0530

    Time: 3 ms (31.83%), Space: 10.4 MB (83.44%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,23 +1,15 @@
 class Solution {
 public:
 void solve(int idx, vector<int>&nums, vector<int>&path, vector<vector<int>>& ans){
-    if(idx==nums.size()){
-        int i=0;
-        while(i<ans.size()){
-            if(path==ans[i]){
-                break;
-            }
-            i++;
+    ans.push_back(path);
+    for(int i=idx;i<nums.size();i++){
+        if(i>idx && nums[i]==nums[i-1]){
+            continue;
         }
-        if(i==ans.size()){
-            ans.push_back(path);
-        }
-        return;
-    }
-        solve(idx+1, nums, path,ans);
-        path.push_back(nums[idx]);
-        solve(idx+1,nums,path,ans);
+        path.push_back(nums[i]);
+        solve(i+1,nums,path,ans);
         path.pop_back();
+    }
 }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         vector<int> path;

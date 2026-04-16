90d4a06efab2be1054c1af2f43c1b0210ea91558
Palak <161704641+Palak-0704@users.noreply.github.com>
2026-04-17 04:25:15 +0530

    Time: 0 ms (100%), Space: 147 MB (22.89%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
@@ -15,13 +15,15 @@ public:
         if(!root){
             return 0;
         }
+        if(!root->left && !root->right){
+            return 1;
+        }
         if(!root->left){
-            return 1+ minDepth(root->right);
+            return 1+minDepth(root->right);
         }
         if(!root->right){
-            return 1+ minDepth(root->left);
+            return 1+minDepth(root->left);
         }
-
-        return 1+ min(minDepth(root->left), minDepth(root->right));
+        return 1+ min(minDepth(root->left), minDepth(root->right)); 
     }
 };
